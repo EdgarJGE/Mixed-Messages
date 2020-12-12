e12f9e15555a8d3602cebd6c299d3cba69ea8c8b
EdgarJGE <EdgarJGE@users.noreply.github.com>
2020-12-12 16:59:32 -0600

    Add words array

---
+++ app.js
@@ -0,0 +1,3 @@
+const movies = ['Star Wars', 'The Lord Of the Rings', 'Alien'];
+const snacks = ['Popcorn', 'Nachos', 'Hot Dogs', 'Fruity Candies'];
+const sodas = ['Coca Cola', 'Fuze Tea', 'Manzanita', 'Boing'];
