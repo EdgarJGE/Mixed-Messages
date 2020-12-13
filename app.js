7701200cb43727962a6ff086cfe0f187a2c03406
EdgarJGE <EdgarJGE@users.noreply.github.com>
2020-12-12 19:33:18 -0600

    Generate random message

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
-const movies = ['Star Wars', 'The Lord Of the Rings', 'Alien'];
-const snacks = ['Popcorn', 'Nachos', 'Hot Dogs', 'Fruity Candies'];
-const sodas = ['Coca Cola', 'Fuze Tea', 'Manzanita', 'Boing'];
+//Generate random number
+const randomNumber = item => {
+    return Math.floor(Math.random() * item.length);
+}
+
+//Cinema night object
+const cinemaNight = {
+    movies: ['Star Wars', 'The Lord Of the Rings', 'Alien', 'The Hobbit'],
+    snacks: ['Popcorn', 'Nachos', 'Hot Dogs', 'Fruity Candies'],
+    sodas: ['Coca Cola', 'Fuze Tea', 'Manzanita', 'Boing'],
+    randomMovie () {
+        return this.movies[randomNumber(this.movies)];
+    },
+    randomSnack () {
+        return this.snacks[randomNumber(this.snacks)];
+    },
+    randomSoda () {
+        return this.sodas[randomNumber(this.sodas)];
+    }
+}
+
+//Generate the random message
+const randomMessage = () => {
+    const newMessage = [];
+    newMessage.push(`Watch this movie: ${cinemaNight.randomMovie()}`);
+    newMessage.push(`Eat this snack: ${cinemaNight.randomSnack()}`);
+    newMessage.push(`Drink this soda: ${cinemaNight.randomSoda()}`);
+    newMessage.forEach(str => {
+        console.log(str);
+    });
+}
+
+//Function call
+randomMessage();
